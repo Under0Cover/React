c5b734a6985b50167c91aa8ea5ee65b763118677
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2020-12-08 23:20:51 -0300

    Aula 02 - React

---
+++ celketutorial/aula_02/Headers.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Headers extends React.Component {
+    render() {
+        return <h2>Cabeçalho</h2>
+    }
+}
+
+// Com mais de uma linha no return:
+/* class Headers extends React.Component {
+    render() {
+        return (
+            <h1>Cabeçalho</h1>
+            );
+    }
+}*/
+
+export default Headers;
